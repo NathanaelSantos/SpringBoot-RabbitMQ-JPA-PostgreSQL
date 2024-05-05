9ec45005a424d590fa6f8a6abd15ce8275811759
Nathan Santos <ti.natan@itpi.org.br>
2024-05-05 01:06:50 -0300

    Added class UserProducer

---
+++ user/user/src/main/java/com/ms/user/producers/UserProducer.java
@@ -0,0 +1,29 @@
+package com.ms.user.producers;
+
+import com.ms.user.dtos.EmailDto;
+import com.ms.user.models.UserModel;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserProducer {
+    final RabbitTemplate rabbitTemplate;
+
+    public UserProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Value(value = "${broker.queue.email.name}")
+    private String routingKey;
+
+    public void publishMessageEmail(UserModel userModel){
+        var emailDto = new EmailDto();
+        emailDto.setUserId(userModel.getUserId());
+        emailDto.setEmailTo(userModel.getEmail());
+        emailDto.setSubject("Registration successful!");
+        emailDto.setText(userModel.getName() + ", Welcome! \n Enjoy all the features of our platform now");
+
+        rabbitTemplate.convertAndSend("", routingKey, emailDto);
+    }
+}
