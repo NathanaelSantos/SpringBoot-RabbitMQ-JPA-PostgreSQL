e2c222509eedec45bd98c3ca2fbf6aac84bfbb53
Nathan Santos <ti.natan@itpi.org.br>
2024-05-05 01:07:41 -0300

    Added publishMessageEmail

--- user/user/src/main/java/com/ms/user/services/UserService.java
+++ user/user/src/main/java/com/ms/user/services/UserService.java
@@ -1,22 +1,27 @@
 package com.ms.user.services;
 
 import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
 import com.ms.user.repositories.UserRepository;
 
-import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
     final UserRepository userRepository;
+    final UserProducer userProducer;
 
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository, UserProducer userProducer){
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
 
     @Transactional
     public UserModel save(UserModel userModel){
-        return userRepository.save(userModel);
+        userModel = userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+
+        return userModel;
     }
 }
