9672f623acc98cd940607b22ce5b4bdb99f9f283
Nathan Santos <ti.natan@itpi.org.br>
2024-05-05 01:07:09 -0300

    Added class RabbitMQConfig

---
+++ user/user/src/main/java/com/ms/user/configs/RabbitMQConfig.java
@@ -0,0 +1,14 @@
+package com.ms.user.configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter(new ObjectMapper());
+    }
+}
